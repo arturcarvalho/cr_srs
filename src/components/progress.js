9069a39223a4fc9cf13f6acf56107991e5c0a7ee
Artur Carvalho <arturbelico@gmail.com>
2019-05-12 12:01:49 +0200

    add progress squares

--- src/components/progress.js
+++ src/components/progress.js
@@ -1,14 +1,28 @@
 import React from "react"
 
 const Progress = ({ resetProgress, totalCards, correctCardsCount }) => {
+  const cardsMissing = totalCards - correctCardsCount
+  const squares = []
+
+  for (let i = 0; i < correctCardsCount; i++) {
+    squares.push(<div key={"c" + i} className="correct-square" />)
+  }
+
+  for (let i = 0; i < cardsMissing; i++) {
+    squares.push(<div key={"m" + i} className="missing-square" />)
+  }
+
   return (
     <>
-      <h2>Your progress:</h2>
-      <div>
-        {correctCardsCount} / {totalCards}
-      </div>
+      <h2>
+        Your progress ({correctCardsCount} / {totalCards}):
+      </h2>
+      <div className="progress-squares">{squares}</div>
+
       {correctCardsCount > 0 && (
-        <button className="reset-btn" onClick={resetProgress}>Reset Progress</button>
+        <button className="reset-btn" onClick={resetProgress}>
+          Reset Progress
+        </button>
       )}
     </>
   )
