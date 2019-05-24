9a3a148b5a4e3f63010228e26dc1db1fe34ed356
Artur Carvalho <arturbelico@gmail.com>
2019-05-24 10:42:28 +0200

    add redux devtools support

--- src/store/ReduxWrapper.js
+++ src/store/ReduxWrapper.js
@@ -7,7 +7,22 @@ import rootReducer from "./rootReducer"
 import { loadState, saveState } from "./localStorage"
 
 const persistedState = loadState()
-const store = reduxCreateStore(rootReducer, persistedState)
+
+// https://github.com/gatsbyjs/gatsby/issues/6137
+// check if window exists, if not
+// pass the identity function (enhancer needs to be a function)
+
+const windowGlobal = typeof window !== "undefined" && window
+
+const devTools =
+  process.env.NODE_ENV === "development" && windowGlobal.devToolsExtension
+    ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+    : f => f
+
+/* eslint-disable no-underscore-dangle */
+const store = reduxCreateStore(rootReducer, persistedState, devTools)
+/* eslint-enable */
 
 // update localstore at most once per second
 store.subscribe(
