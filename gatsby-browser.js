4738d9c316940dde735fa8f369ca0f8a6e8b6d6e
Artur Carvalho <arturbelico@gmail.com>
2019-07-12 15:24:46 +0200

    Change code color to default one

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,5 @@
 // custom typefaces
 import "typeface-montserrat"
 import "typeface-merriweather"
-import "prismjs/themes/prism-okaidia.css"
+import "prismjs/themes/prism.css"
 export { default as wrapRootElement } from "./src/store/ReduxWrapper"
